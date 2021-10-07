a13210b6eeb3cb939e63f712108d019d0a4db807
rodrigoeasilva <rodrigoeasilva@gmail.com>
2021-10-07 12:50:58 -0300

    Script padrão para criação de dominio
    Para ambientes Windows em infraestrutura local

---
+++ createDomain.ps1
@@ -0,0 +1,18 @@
+﻿#
+# Script do Windows PowerShell para Implantação do AD DS
+# -DomainNetbiosName colocar em caixa alta sempre de acordo com o primeiro nome de DomainName(Meu padrão de trabalho)
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "Win2012R2" `
+-DomainName "agencia.local" `
+-DomainNetbiosName "AGENCIA" `
+-ForestMode "Win2012R2" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
